8782bcdcc6a05822109d870f91fdf9b102980a0c
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-06-25 18:34:10 +0100

    Create Wrappe.js

---
+++ Wrappe.js
@@ -0,0 +1,18 @@
+//Wrappe.js
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Wrapper = ({ children }) => {
+  return (
+    <div className="wrapper">
+      {children}
+    </div>
+  );
+};
+
+Wrapper.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Wrapper;
